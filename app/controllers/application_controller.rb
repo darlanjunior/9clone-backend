b76b68aa53bb3f9876c7bd3eae8f4126e9d1eab5
Darlan Junior <darlan.sbjr@gmail.com>
2017-08-05 22:14:32 -0300

    Handle generic active record errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ::NameError, with: :error_occurred
+  rescue_from ::ActiveRecord::ActiveRecordError, with: :error_occurred
+  rescue_from ::ActionController::RoutingError, with: :error_occurred
+
+  protected
+  def error_occurred(exception)
+    logger.error "Error occurred: #{exception}"
+    logger.error "Params: #{params}"
+
+    render json: {error: exception.message}.to_json, status: 500
+    return
+  end
 end
