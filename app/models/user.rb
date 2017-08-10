f2d203f5cc4d53dbd976320bc8597ca360e9dd3e
Darlan Junior <darlan.sbjr@gmail.com>
2017-08-09 23:01:53 -0300

    Add picture uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :confirmable, :recoverable
   include DeviseTokenAuth::Concerns::User
+  mount_uploader :picture, PictureUploader
 end
